d6eb750fb3474c3c7e57006f61b2ee900dfa63f0
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-04 12:02:54 -0600

    setting up index to run server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it
+require('dotenv').config()
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+    console.log(`server running ${PORT}`)
+})
